f43d753f8d3e6f2737b1042fa60132a7d177616d
Oleksandr Diachenko <dyachenko664@gmail.com>
2025-05-21 16:17:22 +0300

    add basic components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, computed, Signal } from '@angular/core';
+import { SearchComponent } from './components/search/search.component';
+import { WeatherCardComponent } from "./components/weather-card/weather-card.component";
+import { ApiWeatherResponse } from './models/weather.model';
+import { ButtonModule } from 'primeng/button';
+import { CurrentWeatherObj, DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SearchComponent, WeatherCardComponent, WeatherCardComponent, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'weather-forecast';
+  foundForecast: any = null;
+  favouriteLocations: Signal<any[]> = computed(() => this.dataService.favouriteLocations())
+
+  constructor(private dataService: DataService) {}
+
+  addToLocations(): void {
+  }
+
+  searchedCity(event: CurrentWeatherObj): void {
+    this.foundForecast = event;
+  }
 }
